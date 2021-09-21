6191aa86d351ee4a3fc1997c53bebafd1a6ea570
Jeff Cela <jeff@Jeffs-iMac.attlocal.net>
2021-09-20 22:39:37 -0400

    added my required items to the index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Required items
+const inquirer = require('inquirer');
+const mysql = require("mysql2");
+const logo = require("asciiart-logo");
+const consoleTable = require("console.table");
+const config = require("./package.json");
